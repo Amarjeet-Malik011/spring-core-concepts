41c7f581b74d9fbd52ba4fb36b2e29743941f7ab
kavita <kavita.bora@tothenew.com>
2017-07-19 19:57:11 +0530

    basic structure created for spring-core concepts.

---
+++ src/main/java/ioc_container_springbean/Application.java
@@ -0,0 +1,5 @@
+package ioc_container_springbean;
+
+
+public class Application {
+}
---
+++ src/main/java/springbean_autowiring/Application.java
@@ -0,0 +1,9 @@
+package springbean_autowiring;
+
+
+public class Application {
+
+    public static void main(String ...arg){
+        System.out.println("Starte");
+    }
+}
